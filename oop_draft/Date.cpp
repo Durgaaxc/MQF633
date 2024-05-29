ffdacb725d97b6199108591b7f77003ddd258636
Durgaaxc <160998834+Durgaaxc@users.noreply.github.com>
2024-05-29 22:21:18 +0800

    Delete oop_draft directory

--- oop_draft/Date.cpp
+++
@@ -1,8 +0,0 @@
-#include "Date.h"
-
-double operator-(const Date& d1, const Date& d2) {
-    int yearDiff = d1.year - d2.year;
-    int monthDiff = (d1.month - d2.month);
-    int dayDiff = d1.day - d2.day;
-    return yearDiff + monthDiff / 12.0 + dayDiff / 365.0;
-}
